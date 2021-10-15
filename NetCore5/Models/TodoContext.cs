7a4b7332ad5ac76e1893e85f1ed3677556dad994
beardemo11 <a22031227@gmail.com>
2021-10-16 02:05:56 +0800

    加入專案檔案。

---
+++ NetCore5/Models/TodoContext.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace NetCore5.Models
+{
+    public partial class TodoContext : DbContext
+    {
+        public TodoContext()
+        {
+        }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Division> Divisions { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<JobTitle> JobTitles { get; set; }
+        public virtual DbSet<TodoList> TodoLists { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Todo.mdf;Integrated Security=True;Connect Timeout=30");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Division>(entity =>
+            {
+                entity.ToTable("Division");
+
+                entity.Property(e => e.DivisionId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmployeeId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Account).IsRequired();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.HasOne(d => d.Division)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.DivisionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employee_ToTable_1");
+
+                entity.HasOne(d => d.JobTitle)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.JobTitleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employee_ToTable");
+            });
+
+            modelBuilder.Entity<JobTitle>(entity =>
+            {
+                entity.ToTable("JobTitle");
+
+                entity.Property(e => e.JobTitleId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<TodoList>(entity =>
+            {
+                entity.HasKey(e => e.TodoId)
+                    .HasName("PK__Table__95862552FC49C675");
+
+                entity.ToTable("TodoList");
+
+                entity.Property(e => e.TodoId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.InsertTime)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.Property(e => e.UpdateTime)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.HasOne(d => d.InsertEmployee)
+                    .WithMany(p => p.TodoListInsertEmployees)
+                    .HasForeignKey(d => d.InsertEmployeeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Todo_ToTable");
+
+                entity.HasOne(d => d.UpdateEmployee)
+                    .WithMany(p => p.TodoListUpdateEmployees)
+                    .HasForeignKey(d => d.UpdateEmployeeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Todo_ToTable_1");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
